0cf02531d0bcbb2ab3ea7af096dce9e1ceee9c2f
Damian Aquino <118084592+DamianAquino@users.noreply.github.com>
2025-05-15 19:37:24 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,34 +1,33 @@
 #include "headers.h"
 //#include "funciones.c"
 int main(void) {
-    Network_dir server_dir;
-    Network_dir cliente_dir;
+    Network_dir server_fd;
+    Network_dir cliente_fd;
 
-    if(crear_server(&server_dir))
+    if(crear_server(&server_fd))
         return -1;
 
     if(!set_root())
         return -2;
 
-    int n = 0, descriptores[MAX_CLIENTS];
-    socklen_t cliente_len = sizeof(cliente_dir.address);
+    int n = 0, descriptores[MAX_FDS];
+    socklen_t cliente_len = sizeof(cliente_fd.address);
     fd_set readfds;
 
     while(1)
     {
         FD_ZERO(&readfds);
-        FD_SET(server_dir.fd, &readfds);
+        FD_SET(server_fd.fd, &readfds);
         set_fds(descriptores, n, &readfds);
-        select(MAX_CLIENTS, &readfds, NULL, NULL, NULL);
+        select(MAX_FDS, &readfds, NULL, NULL, NULL);
 
         // Verifica si se recibio una conexion o un cliente envio datos
-        if(FD_ISSET(server_dir.fd, &readfds))
-            set_cliente(server_dir.fd, &cliente_dir, &cliente_len, &n, descriptores);
-
+        if(FD_ISSET(server_fd.fd, &readfds))
+            set_cliente(server_fd.fd, &cliente_fd, &cliente_len, &n, descriptores);
         else
             leer_stream(&readfds, descriptores, &n);
     }
 
-    close(server_dir.fd);
+    close(server_fd.fd);
     return 0;
 }
